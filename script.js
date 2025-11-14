2037639df938a481b4bf4a22bf6d6829099816d4
Theosaurus-Rex <theosaurus13@gmail.com>
2025-11-15 08:09:10 +1100

    feat: Make pokemon cards clickable in order to link to details views

--- script.js
+++ script.js
@@ -76,13 +76,16 @@ function getPokemonIdFromUrl(url) {
 
 // Create a Pokemon card element for the index list view
 function createPokemonCard(pokemon) {
-  const card = document.createElement("div");
+  const card = document.createElement("a");
+  card.href = `#/pokemon/${pokemon.id}`;
   card.className = "pokemon-card";
+
   card.innerHTML = `
     <img class="pokemon-image" src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
     <h2 class="pokemon-name">${pokemon.name}</h2>
     <p class="pokemon-id">#${pokemon.id.toString().padStart(3, "0")}</p>
   `;
+
   return card;
 }
 
